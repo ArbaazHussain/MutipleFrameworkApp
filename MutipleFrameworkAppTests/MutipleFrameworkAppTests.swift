5f96768707e037acfe5cd37901622174fee022bc
Arbaaz Hussain <Arbaaz.Hussain@telekom-digital.com>
2025-06-14 13:48:39 +0530

    CI-CD setup

---
+++ MutipleFrameworkAppTests/MutipleFrameworkAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MutipleFrameworkAppTests.swift
+//  MutipleFrameworkAppTests
+//
+//  Created by Arbaaz Hussain on 13/06/25.
+//
+
+import XCTest
+@testable import MutipleFrameworkApp
+
+final class MutipleFrameworkAppTests: XCTestCase {
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        XCTAssertEqual(2*2, 4)
+    }
+}
